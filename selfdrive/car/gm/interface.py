deb3465d1b6720bd65c2651a40d028ea1e9ac177
Comma Device <device@comma.ai>
2021-08-23 22:41:10 +0900

    dragonpilot0.8.7-3

---
+++ selfdrive/car/gm/interface.py
@@ -0,0 +1,233 @@
+#!/usr/bin/env python3
+from cereal import car
+from cereal import log
+from common.numpy_fast import interp
+from selfdrive.config import Conversions as CV
+from selfdrive.car.gm.values import CAR, Ecu, ECU_FINGERPRINT, CruiseButtons, \
+                                    AccState, FINGERPRINTS
+from selfdrive.car import STD_CARGO_KG, scale_rot_inertia, scale_tire_stiffness, is_ecu_disconnected, gen_empty_fingerprint
+from selfdrive.car.interfaces import CarInterfaceBase
+from common.dp_common import common_interface_atl, common_interface_get_params_lqr
+FOLLOW_AGGRESSION = 0.15 # (Acceleration/Decel aggression) Lower is more aggressive
+
+ButtonType = car.CarState.ButtonEvent.Type
+EventName = car.CarEvent.EventName
+
+class CarInterface(CarInterfaceBase):
+
+  @staticmethod
+  def compute_gb(accel, speed):
+  	# Ripped from compute_gb_honda in Honda's interface.py. Works well off shelf but may need more tuning
+    creep_brake = 0.0
+    creep_speed = 2.68
+    creep_brake_value = 0.10
+    if speed < creep_speed:
+      creep_brake = (creep_speed - speed) / creep_speed * creep_brake_value
+    return float(accel) / 4.8 - creep_brake
+
+  @staticmethod
+  @staticmethod
+  def get_params(candidate, fingerprint=gen_empty_fingerprint(), has_relay=False, car_fw=None):
+    ret = CarInterfaceBase.get_std_params(candidate, fingerprint, has_relay)
+    ret.carName = "gm"
+    # dp
+    ret.lateralTuning.init('pid')
+    ret.safetyModel = car.CarParams.SafetyModel.gm
+    ret.pcmCruise = False  # stock cruise control is kept off
+
+    # GM port is a community feature
+    # TODO: make a port that uses a car harness and it only intercepts the camera
+    ret.communityFeature = True
+
+    # Presence of a camera on the object bus is ok.
+    # Have to go to read_only if ASCM is online (ACC-enabled cars),
+    # or camera is on powertrain bus (LKA cars without ACC).
+    ret.enableCamera = is_ecu_disconnected(fingerprint[0], FINGERPRINTS, ECU_FINGERPRINT, candidate, Ecu.fwdCamera) or has_relay
+    ret.openpilotLongitudinalControl = ret.enableCamera
+    tire_stiffness_factor = 0.444  # not optimized yet
+
+    # Start with a baseline lateral tuning for all GM vehicles. Override tuning as needed in each model section below.
+    ret.minSteerSpeed = 7 * CV.MPH_TO_MS
+    ret.lateralTuning.pid.kiBP, ret.lateralTuning.pid.kpBP = [[0.], [0.]]
+    ret.lateralTuning.pid.kpV, ret.lateralTuning.pid.kiV = [[0.192], [0.021]]
+    ret.lateralTuning.pid.kf = 0.00006   # full torque for 20 deg at 80mph means 0.00007818594
+    ret.steerRateCost = 0.465
+    ret.steerActuatorDelay = 0.3  # Default delay, not measured yet
+
+    if candidate == CAR.VOLT:
+      # supports stop and go, but initial engage must be above 18mph (which include conservatism)
+      ret.minEnableSpeed = -1 * CV.MPH_TO_MS
+      ret.mass = 1607. + STD_CARGO_KG
+      ret.wheelbase = 2.69
+      ret.steerRatio = 15.7
+      ret.steerRatioRear = 0.
+      ret.centerToFront = ret.wheelbase * 0.4  #  wild guess
+      ret.lateralTuning.pid.kpV, ret.lateralTuning.pid.kiV = [[0.192], [0.021]]
+      ret.lateralTuning.pid.kf = 0.00006   # full torque for 20 deg at 80mph means 0.00007818594
+      ret.steerRateCost = 0.465
+      ret.steerActuatorDelay = 0.2  # Default delay, not measured yet	  
+
+    elif candidate == CAR.MALIBU:
+      # supports stop and go, but initial engage must be above 18mph (which include conservatism)
+      ret.minEnableSpeed = 18 * CV.MPH_TO_MS
+      ret.mass = 1496. + STD_CARGO_KG
+      ret.wheelbase = 2.83
+      ret.steerRatio = 15.8
+      ret.steerRatioRear = 0.
+      ret.centerToFront = ret.wheelbase * 0.4  # wild guess
+
+    elif candidate == CAR.HOLDEN_ASTRA:
+      ret.mass = 1363. + STD_CARGO_KG
+      ret.wheelbase = 2.662
+      # Remaining parameters copied from Volt for now
+      ret.centerToFront = ret.wheelbase * 0.4
+      ret.minEnableSpeed = 18 * CV.MPH_TO_MS
+      ret.steerRatio = 15.7
+      ret.steerRatioRear = 0.
+
+    elif candidate == CAR.ACADIA:
+      ret.minEnableSpeed = -1.  # engage speed is decided by pcm
+      ret.mass = 4353. * CV.LB_TO_KG + STD_CARGO_KG
+      ret.wheelbase = 2.86
+      ret.steerRatio = 14.4  # end to end is 13.46
+      ret.steerRatioRear = 0.
+      ret.centerToFront = ret.wheelbase * 0.4
+
+    elif candidate == CAR.BUICK_REGAL:
+      ret.minEnableSpeed = 18 * CV.MPH_TO_MS
+      ret.mass = 3779. * CV.LB_TO_KG + STD_CARGO_KG  # (3849+3708)/2
+      ret.wheelbase = 2.83  # 111.4 inches in meters
+      ret.steerRatio = 14.4  # guess for tourx
+      ret.steerRatioRear = 0.
+      ret.centerToFront = ret.wheelbase * 0.4  # guess for tourx
+
+    elif candidate == CAR.CADILLAC_ATS:
+      ret.minEnableSpeed = 18 * CV.MPH_TO_MS
+      ret.mass = 1601. + STD_CARGO_KG
+      ret.wheelbase = 2.78
+      ret.steerRatio = 15.3
+      ret.steerRatioRear = 0.
+      ret.centerToFront = ret.wheelbase * 0.49
+
+    # TODO: get actual value, for now starting with reasonable value for
+    # civic and scaling by mass and wheelbase
+    ret.rotationalInertia = scale_rot_inertia(ret.mass, ret.wheelbase)
+
+    # TODO: start from empirically derived lateral slip stiffness for the civic and scale by
+    # mass and CG position, so all cars will have approximately similar dyn behaviors
+    ret.tireStiffnessFront, ret.tireStiffnessRear = scale_tire_stiffness(ret.mass, ret.wheelbase, ret.centerToFront,
+                                                                         tire_stiffness_factor=tire_stiffness_factor)
+
+    ret.stoppingControl = True
+
+
+    ret.longitudinalTuning.deadzoneBP = [0., 8.05]
+    ret.longitudinalTuning.deadzoneV = [.0, .14]
+
+    ret.longitudinalTuning.kpBP = [0., 5., 10., 20.]
+    ret.longitudinalTuning.kpV = [1.8, 2.7, 4.0, 3.0]
+    ret.longitudinalTuning.kiBP = [0., 3., 7., 12., 20., 27.]
+    ret.longitudinalTuning.kiV = [.05, .07, .09, .15, .13, .1]
+
+    ret.stoppingBrakeRate = 0.1 # reach stopping target smoothly
+    ret.startingBrakeRate = 2.0 # release brakes fast
+    ret.startAccel = 1.2 # Accelerate from 0 faster
+
+    ret.steerLimitTimer = 0.4
+    ret.radarTimeStep = 0.0667  # GM radar runs at 15Hz instead of standard 20Hz
+
+    # dp
+    ret = common_interface_get_params_lqr(ret)
+
+    return ret
+
+  # returns a car.CarState
+  def update(self, c, can_strings, dragonconf):
+    self.cp.update_strings(can_strings)
+    # bellow two lines for Brake Light
+    self.cp_chassis.update_strings(can_strings)
+    ret = self.CS.update(self.cp, self.cp_chassis)
+    # dp
+    self.dragonconf = dragonconf
+    ret.cruiseState.enabled = common_interface_atl(ret, dragonconf.dpAtl)
+    ret.canValid = self.cp.can_valid
+    ret.steeringRateLimited = self.CC.steer_rate_limited if self.CC is not None else False
+
+    buttonEvents = []
+
+    if self.CS.cruise_buttons != self.CS.prev_cruise_buttons and self.CS.prev_cruise_buttons != CruiseButtons.INIT:
+      be = car.CarState.ButtonEvent.new_message()
+      be.type = ButtonType.unknown
+      if self.CS.cruise_buttons != CruiseButtons.UNPRESS:
+        be.pressed = True
+        but = self.CS.cruise_buttons
+      else:
+        be.pressed = False
+        but = self.CS.prev_cruise_buttons
+      if but == CruiseButtons.RES_ACCEL:
+        if not (ret.cruiseState.enabled and ret.standstill):
+          be.type = ButtonType.accelCruise  # Suppress resume button if we're resuming from stop so we don't adjust speed.
+      elif but == CruiseButtons.DECEL_SET:
+        be.type = ButtonType.decelCruise
+      elif but == CruiseButtons.CANCEL:
+        be.type = ButtonType.cancel
+      elif but == CruiseButtons.MAIN:
+        be.type = ButtonType.altButton3
+      buttonEvents.append(be)
+
+    ret.buttonEvents = buttonEvents
+
+    events = self.create_common_events(ret, pcm_enable=False)
+
+    if ret.vEgo < self.CP.minEnableSpeed:
+      events.add(EventName.belowEngageSpeed)
+    if self.CS.park_brake:
+      events.add(EventName.parkBrake)
+
+    if self.CS.autoHoldActivated:
+      events.add(car.CarEvent.EventName.autoHoldActivated)
+    if ret.cruiseState.standstill:
+      events.add(EventName.resumeRequired)
+
+    # handle button presses
+    for b in ret.buttonEvents:
+      # do enable on both accel and decel buttons
+      if b.type in [ButtonType.accelCruise, ButtonType.decelCruise] and not b.pressed:
+        events.add(EventName.buttonEnable)
+      # do disable on button down
+      if b.type == ButtonType.cancel and b.pressed:
+        events.add(EventName.buttonCancel)
+
+    ret.events = events.to_msg()
+
+    # copy back carState packet to CS
+    self.CS.out = ret.as_reader()
+
+    return self.CS.out
+
+  def apply(self, c):
+    hud_v_cruise = c.hudControl.setSpeed
+    if hud_v_cruise > 70:
+      hud_v_cruise = 0
+
+    # For Openpilot, "enabled" includes pre-enable.
+    # In GM, PCM faults out if ACC command overlaps user gas.
+    enabled = c.enabled and not self.CS.out.gasPressed
+
+    can_sends = self.CC.update(enabled, self.CS, self.frame,
+                               c.actuators,
+                               hud_v_cruise, c.hudControl.lanesVisible,
+                               c.hudControl.leadVisible, c.hudControl.visualAlert, self.dragonconf)
+
+    self.frame += 1
+
+    # Release Auto Hold and creep smoothly when regenpaddle pressed
+    if self.CS.regenPaddlePressed and self.CS.autoHold:
+      self.CS.autoHoldActive = False
+
+    if self.CS.autoHold and not self.CS.autoHoldActive and not self.CS.regenPaddlePressed:
+      if self.CS.out.vEgo > 0.02:
+        self.CS.autoHoldActive = True
+      elif self.CS.out.vEgo < 0.01 and self.CS.out.brakePressed:
+        self.CS.autoHoldActive = True
+    return can_sends
